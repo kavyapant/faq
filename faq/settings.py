931dbff16b2431b0ec70191e41db228896265f53
Kavya <kavyapant@ymail.com>
2020-04-21 19:11:54 +0530

    s

--- faq/settings.py
+++ faq/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2+5wf%o6l4908=@$qtk@vud)&ezu8*5=31(uu#n6c&c3e1f+22'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-new.herokuapp.com']
 
 
 # Application definition
